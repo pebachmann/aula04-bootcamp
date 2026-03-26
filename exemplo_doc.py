a4a275fa10c6d01a29f5df16890e90a95909fab9
PEdro <pedro.e.bachmann@gmail.com>
2026-03-26 06:24:35 -0300

    Exemplos doc

---
+++ exemplo_doc.py
@@ -0,0 +1,24 @@
+import json
+
+lista = ["sapato", 39, 10.38, True]
+
+produto_1 = {
+    "nome": "Sapato",
+    "quantidade": 39,
+    "preco": 10.38,
+    "disponibilidade": True
+}
+produto_2 = {
+    "nome": "televisao",
+    "quantidade": 10,
+    "preco": 70.38,
+    "disponibilidade": False
+}
+
+carrinho: list = []
+
+carrinho.append(produto_1)
+carrinho.append(produto_2)
+
+carrinho_json = json.dumps(carrinho)
+print(carrinho_json)
---
+++ exemplo_list.py
@@ -0,0 +1,12 @@
+produto: str = "sapato"
+produto_2: str = "camiseta"
+produto_3: str = "videogame"
+
+produtos: list = []
+
+produtos.append(produto)
+produtos.append(produto_2)
+produtos.append(produto_3)
+produtos.pop()
+
+print(produtos)
